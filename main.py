48623ace301706f80bd22d59c8567d273c949ca5
kavinduchamiran <kavindu.chamiran@gmail.com>
2019-06-18 18:52:41 +0530

    working example on local network

--- main.py
+++ main.py
@@ -4,21 +4,34 @@ import sys
 import time
 from random import randint
 
+# Both the classes are implemented in the same file so the first one to run main.py will
+# become the server and the others will automatically be client(s)
+
+# When a server crashes, immediately one of the clients will become the server and other
+# clients will connect to that new server automatically
+
 class Server:
-    connections = []
-    peers = []
+    connections = []    # keep the connections
+    peers = []          # keep the peer ips
 
     def __init__(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-        sock.bind(('0.0.0.0', 10000))
+        sock.bind(('0.0.0.0', 10000))     # binding to local network card, port 10000
         sock.listen(1)
 
         print("Server running")
 
         while True:
+            # there are 2 main methods on Server class
+            # handler() to listen to clients' messages
+            # sendMsg() to listen to own messages
+
+            # main thread will be listning to client connection requests
             c, a = sock.accept()
+
+            # cThread will be running handler()
             cThread = threading.Thread(target=self.handler, args=(c, a))
             cThread.daemon = True
             cThread.start()
@@ -29,26 +42,33 @@ class Server:
             print(str(a[0]) + ':' + str(a[1]), "connected")
             self.sendPeers()
 
+            # iThread will be running sendMsg()
             iThread = threading.Thread(target=self.sendMsg, args=(c,))
             iThread.daemon = True
             iThread.start()
 
     def sendMsg(self, connection):
+        # encode user input to utf-8, convert it to a bitstream and send over the network
         while True:
             connection.send(bytes(input(""), 'utf-8'))
 
     def handler(self, c, a):
         while True:
+            # listen to clients' data streams
+
             data = c.recv(1024)
             sender = bytes(str(a[0]) + ':' + str(a[1]) + " says: ", "utf-8")
 
-            print(data)
+            print(str(a[0]) + ':' + str(a[1]) + " says: ", str(data, 'utf-8'))
 
             for connection in self.connections:
+                # iterate through all active sessions
                 if connection != c:
+                    # no need to send to the same client who sent the message
                     connection.send(sender + data)
 
             if not data:
+                # when there is no data, that means client has been disconnected
                 print(str(a[0]) + ":" + str(a[1]), "disconnected")
                 self.connections.remove(c)
                 self.peers.remove(a[0])
@@ -57,9 +77,11 @@ class Server:
                 break
 
     def sendPeers(self):
+        # update the peer list in every client so they know who their neighbours are
         p = ",".join(self.peers)
 
         for connection in self.connections:
+            # add a special \x11 to distinguish normal messages vs. peer list
             connection.send(b'\x11' + bytes(p, "utf-8"))
 
 
@@ -94,14 +116,15 @@ class Client:
 
 class p2p:
      # this list holds all the peers connected to the server.
-     # initialized to 127.0.0.1 so the first node will start as a server
-     peers = ['192.248.9.138']
+     # set this to other node's ip address
+     peers = ['192.168.43.147']
 
 
 while True:
     try:
         print("Trying to connect...")
 
+        # to avoid race conditions on who becomes the server
         time.sleep(randint(1, 5))
 
         for peer in p2p.peers:
